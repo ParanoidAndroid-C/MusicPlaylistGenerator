32cf32d0f7685e731e47063cb831427e994eb0e8
Mariami Lomtadze <71402417+ParanoidAndroid-C@users.noreply.github.com>
2021-02-03 19:38:48 -0800

    Add files via upload

---
+++ readcsv.py
@@ -0,0 +1,32 @@
+import csv
+import pandas as pd
+
+
+def readData(file_name):
+    tracks = []
+    df = pd.read_csv(file_name)
+    for i in range(0, len(df)):
+        track = []
+        track.append(df['name'][i])
+        track.append(df['album'][i])
+        track.append(df['artist'][i])
+        track.append(df['release_date'][i])
+        track.append(df['length'][i])
+        track.append(df['popularity'][i])
+        track.append(df['danceability'][i])
+        track.append(df['acousticness'][i])
+        track.append(df['energy'][i])
+        track.append(df['instrumentalness'][i])
+        track.append(df['liveness'][i])
+        track.append(df['loudness'][i])
+        track.append(df['speechiness'][i])
+        track.append(df['tempo'][i])
+        track.append(df['time_signature'][i])
+        tracks.append(track)
+    return tracks
+    
+readData('spotify.csv')
+
+
+
+
